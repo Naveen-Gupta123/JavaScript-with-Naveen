817e0aaa360a631f6c0cce28285c25a6a487aa73
Naveen Gupta <naveengupta44787@gmail.com>
2026-06-03 19:30:33 +0530

    Update JavaScript example

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-document.write("Hi Naveen Gupta")
+/*a=15;
+document.write(a);
+function result(){
+    alert("Hello Naveen");
+    alert("Welcome to JavaScript");
+    result();
+}
+*/
+function person1(){
+    alert("Person 1");
+}
+function person2(){
+    alert("Person 2");
+}
+function person3(){
+    alert("Person 3");
+}
+function person4(){
+    alert("Person 4");
+}
